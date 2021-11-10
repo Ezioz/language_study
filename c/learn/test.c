41212c6f517bf22ccfcce3a0d501c1f8a32cb74f
ahtoh <472668561@qq.com>
2021-11-10 16:57:32 +0800

    learn

---
+++ c/learn/test.c
@@ -0,0 +1,19 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: ahtoh
+ * @Date: 2021-11-10 16:51:36
+ * @LastEditors: ahtoh
+ * @LastEditTime: 2021-11-10 16:52:41
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char *s = "hello world";
+    printf("The string is %zd characters long.\n", strlen(s));
+    return 0;
+}
