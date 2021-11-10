c137c6bbc7df730db2b0945382009a8a438c9a69
ahtoh <1655672536@qq.com>
2021-11-10 21:03:32 +0800

    添加子弹

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -4,7 +4,7 @@ version:
 Author: ahtoh
 Date: 2021-11-07 21:33:39
 LastEditors: ahtoh
-LastEditTime: 2021-11-08 21:24:58
+LastEditTime: 2021-11-10 20:56:31
 '''
 
 import sys
@@ -12,6 +12,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     pygame.init()
@@ -19,9 +20,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("alien Invasion")
     ship = Ship(ai_settings, screen)
+    bullets = Group()
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
+        # gf.check_events(ship)
         ship.update()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
         gf.update_screen(ai_settings, screen, ship)
         
 
--- python/alien_invasion/bullet.py
+++ python/alien_invasion/bullet.py
@@ -4,11 +4,29 @@ version:
 Author: ahtoh
 Date: 2021-11-09 22:25:07
 LastEditors: ahtoh
-LastEditTime: 2021-11-09 22:25:08
+LastEditTime: 2021-11-10 20:55:10
 '''
 
 import pygame
 from pygame.sprite import Sprite
 
 class Bullet(Sprite):
-    
+    def __init__(self, ai_settings, screen, ship):
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        self.y = float(self.rect.y)
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        self.y -= self.speed_factor
+        self.rect.y = self.y
+        
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
+
