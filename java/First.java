17fc08e7c40173394fc1f28d11728f3729aba347
ahtoh <1655672536@qq.com>
2021-12-11 14:08:00 +0800

    restart

---
+++ java/First.java
@@ -0,0 +1,18 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: ahtoh
+ * @Date: 2021-12-10 20:51:14
+ * @LastEditors: ahtoh
+ * @LastEditTime: 2021-12-10 21:01:14
+ */
+
+public class First {
+
+  public static void main(String[] args) {
+    String s1 = new String("hello");
+    String s2 = new String("world");
+    String s = s1 + " " + s2;
+    System.out.println(s);
+  }
+}
