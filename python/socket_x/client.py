0df17b025befbf909cf35f94ab326a013df76d5a
ahtoh <1655672536@qq.com>
2021-12-22 23:12:57 +0800

    .

--- python/socket_x/client.py
+++ python/socket_x/client.py
@@ -4,13 +4,34 @@ version:
 Author: ahtoh
 Date: 2021-12-22 09:58:43
 LastEditors: ahtoh
-LastEditTime: 2021-12-22 09:59:56
+LastEditTime: 2021-12-22 23:00:57
 '''
 
-import socket
-s = socket.socket()
-host= socket.gethostname()
-port = 9999
-s.connect((host, port))
-print(s.recv(1024))
-s.close()
+# import socket
+# s = socket.socket()
+# host= socket.gethostname()
+# port = 9999
+# s.connect((host, port))
+# print(s.recv(1024))
+# s.close()
+
+from socket import *
+
+HOST = 'localhost'
+PORT = 12345
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpclient = socket(AF_INET, SOCK_STREAM)
+tcpclient.connect(ADDR)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    tcpclient.send(data)
+    data = tcpclient.recv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode('utf-8'))
+tcpclient.close()
---
+++ python/socket_x/tsTserv.py
@@ -0,0 +1,46 @@
+'''
+Descripttion: 
+version: 
+Author: ahtoh
+Date: 2021-12-22 09:50:26
+LastEditors: ahtoh
+LastEditTime: 2021-12-22 22:28:39
+'''
+# import socket
+
+# tcpSocket = socket.socket()
+# host=  socket.gethostname()
+# port = 12345
+# tcpSocket.bind((host, port))
+
+# tcpSocket.listen(5)
+# while True:
+#     c, addr = tcpSocket.accept()
+#     print('连接地址：', addr)
+#     c.send('welcome')
+#     c.close()
+
+from os import truncate
+from socket import *
+from time import ctime
+
+HOST = ''
+PORT = 12345
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSock = socket(AF_INET, SOCK_STREAM)
+tcpSock.bind(ADDR)
+tcpSock.listen(5)
+
+while True:
+    print('waiting for connection...')
+    tcpclisock, addr = tcpSock.accept()
+    print('...connected from : ', addr)
+    while True:
+        data = tcpclisock.recv(BUFSIZ)
+        if not data:
+            break
+        tcpclisock.send('[%s] %s ' %(bytes(ctime(), 'utf-8'), data))
+    tcpclisock.close()
+tcpSock.close()
