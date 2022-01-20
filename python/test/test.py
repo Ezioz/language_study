91f78732adbcb3a8cf54c20cf7dc332996860957
ahtoh <1655672536@qq.com>
2022-01-20 22:43:58 +0800

    .

--- python/test/test.py
+++ python/test/test.py
@@ -4,13 +4,8 @@ version:
 Author: ahtoh
 Date: 2022-01-04 09:14:47
 LastEditors: ahtoh
-LastEditTime: 2022-01-19 22:54:34
+LastEditTime: 2022-01-20 22:41:25
 '''
 
-users = {
-    'aeinstein': {
-        'first': 'albert',
-        'last': 'einstein',
-        'location': 'princeton',
-    }
-}
+def describe_pet(animal_type, pet_name):
+    
