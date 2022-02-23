52599639242e6a59c640def1829f66b227ff152b
ahtoh <472668561@qq.com>
2022-02-23 11:30:21 +0800

    a

--- c/learn/new/test.c
+++ c/learn/new/test.c
@@ -1,19 +1,40 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: ahtoh
  * @Date: 2022-01-06 19:48:46
  * @LastEditors: ahtoh
- * @LastEditTime: 2022-01-11 19:37:18
+ * @LastEditTime: 2022-02-23 11:26:26
  */
 
+// #include <stdio.h>
+// #include <string.h>
+
+// int main(int argc, char const *argv[])
+// {
+//     char name[40];
+//     scanf("%s", name);
+//     printf("%zd %d", sizeof(name), strlen(name));
+//     return 0;
+// }
+
 #include <stdio.h>
-#include <string.h>
+
+#define MAX_LEN 10
+typedef struct Student
+{
+    long num;
+    char name[MAX_LEN];
+    float sum;
+    struct Student *next;
+} STU;
 
 int main(int argc, char const *argv[])
 {
-    char name[40];
-    scanf("%s", name);
-    printf("%zd %d", sizeof(name), strlen(name));
+    STU *head;
+    head = (STU *)malloc(sizeof(struct Student));
+    scanf("%s", head->name);
+    head->num = 100.0;
+    printf("head -> name %s", head->name);
     return 0;
 }
