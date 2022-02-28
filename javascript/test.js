2d64fdc1c47f90f9b6adde6250d8d075b2a819f5
ahtoh <472668561@qq.com>
2022-02-28 17:13:55 +0800

    .

--- javascript/test.js
+++ javascript/test.js
@@ -4,11 +4,23 @@
  * @Author: ahtoh
  * @Date: 2021-12-29 14:35:21
  * @LastEditors: ahtoh
- * @LastEditTime: 2021-12-29 14:35:22
+ * @LastEditTime: 2022-02-28 09:38:17
  */
 
+/*
 function greetMe(yourname) {
     console.log('hello ' + yourname);
 }
 
-greetMe('world')
+greetMe('world');
+
+var obj = {
+    p : function (x) {
+        return 2 * x;
+    }
+}
+console.log(obj.p(1));
+*/
+var test = 1;
+var test = parseFloat(test);
+console.log(test);
