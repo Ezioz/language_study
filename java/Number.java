8906f8fbb4d188f72e3026368ba008ced82acc4f
ahtoh <1655672536@qq.com>
2022-05-16 11:01:32 +0800

    first commit

--- java/Number.java
+++ java/Number.java
@@ -5,17 +5,17 @@
  * @Author: ahtoh
  * @Date: 2021-12-11 22:11:20
  * @LastEditors: ahtoh
- * @LastEditTime: 2021-12-11 22:12:51
+ * @LastEditTime: 2022-04-07 09:55:49
  */
 
 public class Number {
     public static void main(String[] args) {
         byte mybyte = 24;
-        short myshort= 1234;
+        short myshort = 1234;
         int myint = 1234355;
         long mylong = 23453245;
         long result = mybyte + myshort + myint + mylong;
         System.out.println(result);
     }
-    
+
 }
